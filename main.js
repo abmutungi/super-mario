6931711c4bbb4a8c5248c72bea23efcf470094e2
nsym_coding <nsym.coding@outlook.com>
2022-07-06 18:53:31 +0100

    got mario to jump only twice

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 let position = 99.9;
 let right = 100;
 let jump = 0;
+let bottom = 0;
 let gravity = 0.9;
 let hero = document.getElementById("mario");
 
@@ -27,15 +28,22 @@ const moveLeft = () => {
 };
 
 const moveJump = () => {
-    jump -= 50;
-    //if (isJumping) return;
-    //hero.style.transform = `translateX(${right}px)`;
-    hero.style.transform = `translate(${right}px,${jump}px)`;
-    isJumping = true;
-    jump = jump * gravity;
-    window.requestAnimationFrame(animateScript);
-    console.log("jump");
-    //console.log(hero.offsetTop);
+    if (jumpCount < 3){
+
+        jump -= 50;
+        //if (isJumping) return;
+        //hero.style.transform = `translateX(${right}px)`;
+        hero.style.transform = `translate(${right}px,${jump}px)`;
+        isJumping = true;
+        jump = jump * gravity;
+        window.requestAnimationFrame(animateScript);
+        console.log("jump");
+        //console.log(hero.offsetTop);
+    }else{
+        jump = 0;
+        jumpCount = 0;
+        hero.style.transform = `translate(${right}px, ${jump}px)`
+    }
 };
 
 const control = (e) => {
@@ -45,8 +53,8 @@ const control = (e) => {
         position = 99.9;
     }
     if (e.key === " ") {
-        moveJump();
         jumpCount++;
+        moveJump();
     } else if (e.key === "ArrowLeft") {
         moveLeft();
     } else if (e.key === "ArrowRight") {
