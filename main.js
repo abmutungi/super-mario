40318658d60bd4347f32577a8ee802cd26565191
Arnold Mutungi <abmutungi@gmail.com>
2022-07-06 18:02:09 +0100

    mario getting high

--- main.js
+++ main.js
@@ -1,54 +1,52 @@
 let position = 99.9;
 let right = 100;
-let jump = -50;
+let jump = 0;
+let gravity = 0.9;
 let hero = document.getElementById("mario");
-function animateLateral() {
-    hero.style.backgroundPosition = `-${position}px 0px`;
 
+const animateScript = () => {
+    hero.style.backgroundPosition = `-${position}px 0px`;
+};
+
+const moveRight = () => {
     hero.style.transform = `translateX(${right}px)`;
-    //hero.style.transform = `translateY(${jump}px)`;
+    right += 50;
+    window.requestAnimationFrame(animateScript);
+    console.log("right");
+    console.log(hero.offsetTop);
+};
 
+const moveLeft = () => {
+    hero.style.transform = `translateX(${right}px)`;
+    right -= 50;
+    window.requestAnimationFrame(animateScript);
+    console.log("left");
+    console.log(hero.offsetTop);
+};
 
-    //requestAnimationFrame(animateLateral);
-}
+const moveJump = () => {
+    //hero.style.transform = `translateX(${right}px)`;
+    hero.style.transform = `translate(${right}px,${jump}px)`;
+    jump -= 50;
+    jump = jump * gravity;
+    window.requestAnimationFrame(animateScript);
+    console.log("jump");
+    //console.log(hero.offsetTop);
+};
 
-function animateMedial(){
-    hero.style.backgroundPosition = `-${position}px 0px`;
-    //hero.style.top = jump
-     hero.style.transform = `translateY(${jump}px)`;
-    requestAnimationFrame(animateMedial);
-
-}
-
-console.log(hero.id);
-
-document.addEventListener("keydown", (e) => {
+const control = (e) => {
     if (position < 500) {
         position = position + 99.9;
     } else {
         position = 99.9;
     }
-        if (e.key === "ArrowRight") {
-            window.requestAnimationFrame(animateLateral);
-            
-            right += 50;
-            console.log("move");
-            
-        }
-        if (e.key === "ArrowLeft"){
-            window.requestAnimationFrame(animateLateral);
-            //position = position + 99.9;
-            right -= 50;
-        }
-        if (e.key === " "){
-            hero.style.left = right
-            window.requestAnimationFrame(animateMedial);
-            jump -= 50;
-
-
-        }
-        
-    
-});
-
+    if (e.key === " ") {
+        moveJump();
+    } else if (e.key === "ArrowLeft") {
+        moveLeft();
+    } else if (e.key === "ArrowRight") {
+        moveRight();
+    }
+};
+document.addEventListener("keydown", control);
 //
