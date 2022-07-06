88be2babb709528d828b29eb5ccae198814ec679
nsym_coding <nsym.coding@outlook.com>
2022-07-06 16:46:41 +0100

    some changes

--- main.js
+++ main.js
@@ -1,27 +1,54 @@
 let position = 99.9;
 let right = 100;
+let jump = -50;
 let hero = document.getElementById("mario");
-function animateScript() {
+function animateLateral() {
     hero.style.backgroundPosition = `-${position}px 0px`;
 
     hero.style.transform = `translateX(${right}px)`;
+    //hero.style.transform = `translateY(${jump}px)`;
 
-    requestAnimationFrame(animateScript);
+
+    //requestAnimationFrame(animateLateral);
+}
+
+function animateMedial(){
+    hero.style.backgroundPosition = `-${position}px 0px`;
+    //hero.style.top = jump
+     hero.style.transform = `translateY(${jump}px)`;
+    requestAnimationFrame(animateMedial);
+
 }
 
 console.log(hero.id);
 
 document.addEventListener("keydown", (e) => {
     if (position < 500) {
-        if (e.key === "ArrowRight") {
-            window.requestAnimationFrame(animateScript);
-            position = position + 99.9;
-            right += 50;
-            console.log("move");
-        }
+        position = position + 99.9;
     } else {
         position = 99.9;
     }
+        if (e.key === "ArrowRight") {
+            window.requestAnimationFrame(animateLateral);
+            
+            right += 50;
+            console.log("move");
+            
+        }
+        if (e.key === "ArrowLeft"){
+            window.requestAnimationFrame(animateLateral);
+            //position = position + 99.9;
+            right -= 50;
+        }
+        if (e.key === " "){
+            hero.style.left = right
+            window.requestAnimationFrame(animateMedial);
+            jump -= 50;
+
+
+        }
+        
+    
 });
 
 //
