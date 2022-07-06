5b4c1968d61cac02d716c144d0c5c8ebe680dcf4
Arnold Mutungi <abmutungi@gmail.com>
2022-07-06 14:03:13 +0100

    mario running

--- main.js
+++ main.js
@@ -1,15 +1,27 @@
-let position = 91;
-function animateScript(){
+let position = 99.9;
+let right = 100;
+let hero = document.getElementById("mario");
+function animateScript() {
+    hero.style.backgroundPosition = `-${position}px 0px`;
 
-  document.getElementById('image').style.backgroundPosition = `-${position}px 0px`;
-  requestAnimationFrame(animateScript)
+    hero.style.transform = `translateX(${right}px)`;
+
+    requestAnimationFrame(animateScript);
 }
 
-let hero = document.getElementById('image')
+console.log(hero.id);
 
+document.addEventListener("keydown", (e) => {
+    if (position < 500) {
+        if (e.key === "ArrowRight") {
+            window.requestAnimationFrame(animateScript);
+            position = position + 99.9;
+            right += 50;
+            console.log("move");
+        }
+    } else {
+        position = 99.9;
+    }
+});
 
-
-console.log(hero.id)
-
-// hero.addEventListener('onmouseenter', animateScript())
-window.requestAnimationFrame(animateScript)
+//
