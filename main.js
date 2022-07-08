25558bd3b98d75e014604d335822003a0ff05156
nsym_coding <nsym.coding@outlook.com>
2022-07-08 15:31:43 +0100

    got him to jump somewhat normally but not really smooth

--- main.js
+++ main.js
@@ -5,47 +5,81 @@ let bottom = 0;
 let gravity = 0.9;
 let hero = document.getElementById("mario");
 
+let game = document.getElementById("game");
+console.log(hero.getBoundingClientRect());
+let starttime;
+
 let isJumping = false;
 let jumpCount = 0;
 const animateScript = () => {
     hero.style.backgroundPosition = `-${position}px 0px`;
+    requestAnimationFrame(animateScript)
 };
 
 const moveRight = () => {
-    right += 25;
-    hero.style.transform = `translateX(${right}px)`;
-    window.requestAnimationFrame(animateScript);
-    console.log("right");
-    console.log(hero.offsetTop);
+    
+  if (hero.getBoundingClientRect().right < game.getBoundingClientRect().right-35){
+
+      right += 25;
+      hero.style.transform = `translateX(${right}px)`;
+   
+      console.log("right");
+      console.log(hero.offsetTop);
+  }else{
+    hero.style.left -= 50+ 'px'
+  }
+
+    
 };
 
 const moveLeft = () => {
-    right -= 25;
-    hero.style.transform = `translateX(${right}px) scaleX(-1)`;
-    window.requestAnimationFrame(animateScript);
-    console.log("left");
-    console.log(hero.offsetTop);
+
+    if (hero.getBoundingClientRect().left > game.getBoundingClientRect().left+35){
+
+        right -= 25;
+        hero.style.transform = `translateX(${right}px) scaleX(-1)`;
+       
+        console.log("left");
+        console.log(hero.offsetTop);
+    }else{
+        hero.style.left += 50 + 'px'
+    }
 };
 
 const moveJump = () => {
-    if (jumpCount < 3) {
+if (hero.getBoundingClientRect().bottom < 649 && jumpCount < 3){
+
+    // if (jumpCount < 3){
+
         jump -= 100;
-        //if (isJumping) return;
-        //hero.style.transform = `translateX(${right}px)`;
+        
         hero.style.transform = `translate(${right}px,${jump}px)`;
         isJumping = true;
-        // jump = jump * gravity;
-        window.requestAnimationFrame(animateScript);
+        console.log(hero.getBoundingClientRect());
+        isJumping = true
         console.log("jump");
-        if (hero.style.top < 575){
-            hero.style.transform = `translate(${right}px, 0px)`
-        }
-        //console.log(hero.offsetTop);
-    } else {
+        
+        console.log(hero.offsetTop);
+    }else{
+
         jump = 0;
         jumpCount = 0;
+       // isJumping = false
         hero.style.transform = `translate(${right}px, ${jump}px)`;
     }
+
+
+setTimeout(function(){
+
+    if (isJumping){
+       while (hero.getBoundingClientRect().bottom <648 ){
+        isJumping = false
+        jump *= gravity
+        hero.style.transform = `translate(${right}px,${jump}px)`;
+    
+       }
+    }
+}, 580)
 };
 
 const control = (e) => {
@@ -54,14 +88,18 @@ const control = (e) => {
     } else {
         position = 99.9;
     }
-    if (e.key === " " || e.key === "ArrowUp") {
-        jumpCount++;
-        moveJump();
+    if (e.key === " " ) {
+        jumpCount++;      
+        moveJump();    
     } else if (e.key === "ArrowLeft") {
         moveLeft();
-    } else if (e.key === "ArrowRight") {
+    } else if (e.key === "ArrowRight") {       
         moveRight();
     }
 };
-document.addEventListener("keydown", control);
+requestAnimationFrame(animateScript)
+document.addEventListener("keydown", function(e){
+    control(e)
+});
+
 //
