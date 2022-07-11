2be75a3e9d582eb310e8f252ea958f68ac47ee07
Arnold Mutungi <abmutungi@gmail.com>
2022-07-11 13:51:24 +0100

    merge sliding background

--- main.js
+++ main.js
@@ -4,9 +4,9 @@ let jump = 0;
 let bottom = 0;
 let gravity = 0.9;
 let hero = document.getElementById("mario");
+let game = document.getElementById("game");
 let gameDimensions = game.getBoundingClientRect();
 
-let game = document.getElementById("game");
 console.log(hero.getBoundingClientRect());
 let starttime;
 
@@ -18,82 +18,79 @@ const animateScript = (timestamp) => {
 };
 
 const moveRight = () => {
-    
-  if (hero.getBoundingClientRect().right < game.getBoundingClientRect().right-35){
+    if (
+        hero.getBoundingClientRect().right <
+        game.getBoundingClientRect().right - 35
+    ) {
+        right += 25;
+        hero.style.transform = `translateX(${right}px)`;
 
-      right += 25;
-      hero.style.transform = `translateX(${right}px)`;
-   
-      console.log("right");
-      console.log(hero.offsetTop);
-      gameObject.right = false;
-  }else{
-    hero.style.left -= 50+ 'px'
-  }
-
-    
+        console.log("right");
+        console.log(hero.offsetTop);
+        gameObject.right = false;
+    } else {
+        hero.style.left -= 50 + "px";
+    }
 };
 
 const moveLeft = () => {
-
-    if (hero.getBoundingClientRect().left > game.getBoundingClientRect().left+35){
-
+    if (
+        hero.getBoundingClientRect().left >
+        game.getBoundingClientRect().left + 35
+    ) {
         right -= 25;
         hero.style.transform = `translateX(${right}px) scaleX(-1)`;
-       
+
         console.log("left");
         console.log(hero.offsetTop);
-        gameObject.left = false
-    }else{
-        hero.style.left += 50 + 'px'
+        gameObject.left = false;
+    } else {
+        hero.style.left += 50 + "px";
     }
 };
 
 const moveJump = (timestamp) => {
     console.log(timestamp);
-if (hero.getBoundingClientRect().bottom < 649 && jumpCount < 3){
-
-    // if (jumpCount < 3){
+    if (hero.getBoundingClientRect().bottom < 649 && jumpCount < 3) {
+        // if (jumpCount < 3){
 
         jump -= 100;
-        
+
         hero.style.transform = `translate(${right}px,${jump}px)`;
         isJumping = true;
         console.log(hero.getBoundingClientRect());
-        isJumping = true
+        isJumping = true;
         console.log("jump");
-        
+
         console.log(hero.offsetTop);
         gameObject.jump = false;
-    }else{
-
+    } else {
         jump = 0;
         jumpCount = 0;
-       // isJumping = false
+        // isJumping = false
         hero.style.transform = `translate(${right}px, ${jump}px)`;
         gameObject.jump = false;
     }
 
-
- //setTimeout(function(){
+    //setTimeout(function(){
 
     // if (isJumping){
     //    while (hero.getBoundingClientRect().bottom <648 ){
     //     isJumping = false
     //     jump *= gravity
     //     hero.style.transform = `translate(${right}px,${jump}px)`;
-    
+
     //    }
     // }
-//}, 780)
+    //}, 780)
 };
 
 let gameObject = {
-    "jump": false,
-    "right": false,
-    "left": false,
-    "animate": true,
-}
+    jump: false,
+    right: false,
+    left: false,
+    animate: true,
+};
 
 const control = (e) => {
     if (position < 250) {
@@ -101,15 +98,15 @@ const control = (e) => {
     } else {
         position = 99.9;
     }
-    if (e.key === " " ) {
-        jumpCount++; 
-        //moveJump()     
-        gameObject.jump = true;  
-        console.log(gameObject.true);  
+    if (e.key === " ") {
+        jumpCount++;
+        //moveJump()
+        gameObject.jump = true;
+        console.log(gameObject.true);
     } else if (e.key === "ArrowLeft") {
         //moveLeft();
         gameObject.left = true;
-    } else if (e.key === "ArrowRight") {       
+    } else if (e.key === "ArrowRight") {
         //moveRight();
         gameObject.right = true;
     }
@@ -119,32 +116,30 @@ const control = (e) => {
 //     console.log(timestamp);
 //     animateScript()
 // })
-document.addEventListener("keydown", function(e){
-    control(e)
+document.addEventListener("keydown", function (e) {
+    control(e);
 });
 const gameLoop = () => {
     hero.style.backgroundPosition = `-${position}px 0px`;
-    
-    if (gameObject.jump){
 
-        moveJump()
-
+    if (gameObject.jump) {
+        moveJump();
     }
 
-    if (gameObject.right){
-        moveRight()
+    if (gameObject.right) {
+        moveRight();
     }
 
-    if (gameObject.left){
-        moveLeft()
+    if (gameObject.left) {
+        moveLeft();
     }
 
-    if (gameObject.animate){
-        animateScript()
+    if (gameObject.animate) {
+        animateScript();
     }
-    requestAnimationFrame(gameLoop)
-}
+    requestAnimationFrame(gameLoop);
+};
 
-requestAnimationFrame(gameLoop)
+requestAnimationFrame(gameLoop);
 
 //
